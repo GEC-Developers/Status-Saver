4016797dc630bf744c8bf4c79a8cb070e96e09a6
rawkush <ankushrawat950@gmail.com>
2018-09-24 17:10:44 +0530

    adding a fragment for Displaying images

---
+++ app/src/main/java/com/wedevelopapps/whatsappstatussaver/Fragment/PicDisplay.java
@@ -0,0 +1,26 @@
+package com.wedevelopapps.whatsappstatussaver.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wedevelopapps.whatsappstatussaver.R;
+
+public class PicDisplay extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_pic_display, container, false);
+        return v;
+    }
+
+
+}
