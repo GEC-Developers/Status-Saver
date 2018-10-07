efbd62a47fa5f124208c58976327eec7ecc341f4
Ahraar <ahraralam2012@gmail.com>
2018-10-07 12:10:25 +0530

    some changes

--- app/src/main/java/com/wedevelopapps/whatsappstatussaver/Activity/MainActivity.java
+++ app/src/main/java/com/wedevelopapps/whatsappstatussaver/Activity/MainActivity.java
@@ -25,6 +25,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -86,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mToolBar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolBar);
-        getSupportActionBar().setTitle("WhatsApp Status Saver");
+        getSupportActionBar().setTitle("WA Status Saver");
 
         HelpPopUp = new Dialog(this);
         rateUsPopUp = new Dialog(this);
@@ -214,7 +215,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void ratePopup() {
         rateUsPopUp.setContentView(R.layout.rate_us_pop_up);
-        CardView exitApp = rateUsPopUp.findViewById(R.id.exitApp);
+        Button exitApp = rateUsPopUp.findViewById(R.id.exitApp);
         LinearLayout cancel = rateUsPopUp.findViewById(R.id.cancelButton);
 
         exitApp.setOnClickListener(new View.OnClickListener() {
